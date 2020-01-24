5dc70d7ddc4ba9e64726d0ff6c15b2723331e62a
arash <arash.kalatian@gmail.com>
2020-01-23 19:34:04 -0500

    comments added to the code

--- Models/BasicModel.py
+++ Models/BasicModel.py
@@ -4,13 +4,14 @@ import pandas as pd
 import math
 class BaseModel:
     def __init__(self):
-        self.t = 0.1  # time intervals to be observed
-        self.speed = 1.2  # pedestrian default speed m/s
+        self.t = 0.1                              # time intervals to be observed
+        self.speed = 1.2                          # pedestrian default speed (m/s)
     def predict(self,org,dest):
-        x1, y1 = org
+        x1, y1 = org                              #input: coordiantes of origin and destination
         x2, y2 = dest
-        angle = math.atan2(y2 - y1, x2 - x1)
-        if angle == math.pi / 2:
+        angle = math.atan2(y2 - y1, x2 - x1)      #angle of movement of pedestrian in radians
+
+        if angle == math.pi / 2:                  #speed of ped projected to X and Y axis
             speedX = 0
         else:
             speedX = self.speed * math.cos(angle)
