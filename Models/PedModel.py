9eb9fa65ce88f7398c0a0b87c91938ed73e632b6
arash <arash.kalatian@gmail.com>
2020-01-29 17:10:17 -0500

    minor change in address

--- Models/PedModel.py
+++ Models/PedModel.py
@@ -21,7 +21,7 @@ with open('Model_V0.pkl', 'rb') as f:
 # Load position and context from local text files and convert them to dictionaries,
 # needs to be replaced by !Important desired format
 context = {}
-with open("logs/Context") as f:
+with open("/logs/Context") as f:
     for line in f:
         (key, val) = line.split(' : ')
         context[key] = val[:-1]
@@ -29,7 +29,7 @@ Xn, Yn = map(float, context['Destination'].replace(')', '').replace('(', '').spl
 context['Destination'] = np.array((Xn, Yn))
 
 Positions = {}
-with open("logs/Position") as f:
+with open("/logs/Position") as f:
     for line in f:
         (key, val) = line.split(' : ')
         x, y = map(float, val.replace(')', '').replace('(', '').split(','))
@@ -54,5 +54,5 @@ def ped_model(context, position):
 
 output = ped_model(context, pos)
 
-with open("logs/Position", "a") as f:
+with open("/logs/Position", "a") as f:
     f.write("Position {} : ({:.2f},{:.2f})\n".format(len(Positions), output[0], output[1]))
